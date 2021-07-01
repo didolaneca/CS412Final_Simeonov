b78a232a8304c48e325ffc2ff617081076a7d642
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-07-01 00:24:59 -0500

    Added interfaces for the repositories

---
+++ CS412Final_Simeonov/Repository/Interfaces/IItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using CS412Final_Simeonov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS412Final_Simeonov.Repository.Interfaces
+{
+    interface IItemRepository
+    {
+        Boolean saveItem(Item item);
+
+        Item findItemById(long id);
+
+        Boolean removeItem(long id);
+    }
+}
