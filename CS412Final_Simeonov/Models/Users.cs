8e893305a92109f22d7296be11ae09ad96202c72
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-06-30 22:36:04 -0500

    Added DAL for the user. Added Items model

--- CS412Final_Simeonov/Models/Users.cs
+++ CS412Final_Simeonov/Models/Users.cs
@@ -7,57 +7,119 @@ namespace CS412Final_Simeonov.Models
 {
     public class Users
     {
-        User batman = new User() { 
-            FirstName = "Bruce",
-            LastName = "Wayne",
-            Email = "BruceWayne@gmail.com",
-            Username = "Batman",
-            Password = "IamBatman"
-        };
+        private static List<User> listOfAllUsers;
+        //User batman = new User() { 
+        //    FirstName = "Bruce",
+        //    LastName = "Wayne",
+        //    Email = "BruceWayne@gmail.com",
+        //    Username = "Batman",
+        //    Password = "IamBatman"
+        //};
 
-        User ironMan = new User()
-        {
-            FirstName = "Tony",
-            LastName = "Stark",
-            Email = "TonyStark@gmail.com",
-            Username = "IronMan",
-            Password = "TheBestAvenger"
-        };
+        //User ironMan = new User()
+        //{
+        //    FirstName = "Tony",
+        //    LastName = "Stark",
+        //    Email = "TonyStark@gmail.com",
+        //    Username = "IronMan",
+        //    Password = "TheBestAvenger"
+        //};
 
-        User hulk = new User()
-        {
-            FirstName = "Bruce",
-            LastName = "Banner",
-            Email = "HulkSmash@gmail.com",
-            Username = "Hulk",
-            Password = "NoDoors"
-        };
+        //User hulk = new User()
+        //{
+        //    FirstName = "Bruce",
+        //    LastName = "Banner",
+        //    Email = "HulkSmash@gmail.com",
+        //    Username = "Hulk",
+        //    Password = "NoDoors"
+        //};
 
-        User thor = new User()
-        {
-            FirstName = "Thor",
-            LastName = "OdinSon",
-            Email = "ThorOdinSon@gmail.com",
-            Username = "Thor",
-            Password = "TheStrongestAvenger"
-        };
+        //User thor = new User()
+        //{
+        //    FirstName = "Thor",
+        //    LastName = "OdinSon",
+        //    Email = "ThorOdinSon@gmail.com",
+        //    Username = "Thor",
+        //    Password = "TheStrongestAvenger"
+        //};
 
-        User cptAmerica = new User()
-        {
-            FirstName = "Steve",
-            LastName = "Rogers",
-            Email = "SteveRogers@gmail.com",
-            Username = "CaptainAmerica",
-            Password = "ICanDoThisAllDay"
-        };
+        //User cptAmerica = new User()
+        //{
+        //    FirstName = "Steve",
+        //    LastName = "Rogers",
+        //    Email = "SteveRogers@gmail.com",
+        //    Username = "CaptainAmerica",
+        //    Password = "ICanDoThisAllDay"
+        //};
 
-        User loki = new User()
-        {
-            FirstName = "Loki",
-            LastName = "OdinSon",
-            Email = "LokiOdinSon@gmail.com",
-            Username = "Loki",
-            Password = "IWillNeverDoItAgain"
-        };
+        //User loki = new User()
+        //{
+        //    FirstName = "Loki",
+        //    LastName = "OdinSon",
+        //    Email = "LokiOdinSon@gmail.com",
+        //    Username = "Loki",
+        //    Password = "IWillNeverDoItAgain"
+        //};
+
+        public Users() { 
+            this.listOfAllUsers = new List<User>();
+            User batman = new User()
+            {
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                Email = "BruceWayne@gmail.com",
+                Username = "Batman",
+                Password = "IamBatman"
+            };
+            User ironMan = new User()
+            {
+                FirstName = "Tony",
+                LastName = "Stark",
+                Email = "TonyStark@gmail.com",
+                Username = "IronMan",
+                Password = "TheBestAvenger"
+            };
+            User hulk = new User()
+            {
+                FirstName = "Bruce",
+                LastName = "Banner",
+                Email = "HulkSmash@gmail.com",
+                Username = "Hulk",
+                Password = "NoDoors"
+            };
+            User thor = new User()
+            {
+                FirstName = "Thor",
+                LastName = "OdinSon",
+                Email = "ThorOdinSon@gmail.com",
+                Username = "Thor",
+                Password = "TheStrongestAvenger"
+            };
+            User cptAmerica = new User()
+            {
+                FirstName = "Steve",
+                LastName = "Rogers",
+                Email = "SteveRogers@gmail.com",
+                Username = "CaptainAmerica",
+                Password = "ICanDoThisAllDay"
+            };
+            User loki = new User()
+            {
+                FirstName = "Loki",
+                LastName = "OdinSon",
+                Email = "LokiOdinSon@gmail.com",
+                Username = "Loki",
+                Password = "IWillNeverDoItAgain"
+            };
+            listOfAllUsers.Add(loki);
+            listOfAllUsers.Add(cptAmerica);
+            listOfAllUsers.Add(thor);
+            listOfAllUsers.Add(hulk);
+            listOfAllUsers.Add(ironMan);
+            listOfAllUsers.Add(batman);
+        }
+        public static List<User> getAllUsers() {
+            return listOfAllUsers;
+        }
     }
 }
