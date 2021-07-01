49104003bb82c562326bf4e6fb8a11776a6829b9
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-06-30 22:56:34 -0500

    Added fields to the item model

--- CS412Final_Simeonov/Models/Item.cs
+++ CS412Final_Simeonov/Models/Item.cs
@@ -7,5 +7,14 @@ namespace CS412Final_Simeonov.Models
 {
     public class Item
     {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Desciption { get; set; }
+
+        public string Price { get; set; }
+
+
+        //To DO replace Sting with the class containingn the images.
+        public List<String> Images { get; set; }
     }
 }
