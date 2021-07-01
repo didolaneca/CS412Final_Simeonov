3f2b7777dad3882e5eaacfb1d5d1d459236c4b95
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-07-01 01:49:11 -0500

    Added order and item repositories and implementation

--- CS412Final_Simeonov/DAL/OrderDAL.cs
+++ CS412Final_Simeonov/DAL/OrderDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CS412Final_Simeonov.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,23 @@ namespace CS412Final_Simeonov.DAL
 {
     public class OrderDAL
     {
+        static List<Order> allOrders;
+        public OrderDAL()
+        {
+            allOrders = new List<Order>();
+            //Item mac = new Item();
+            //mac.Id.set(1); = 1;
+            Item itemOne = ItemDAL.GetItemById(1);
+
+            allOrders.Add(new Order() { 
+                Id = 1,
+                Items = new List<Item>() { itemOne },
+                //Can I access the Items List from the Order Object????
+                TotalAmount = itemOne.Price
+            });
+
+        }
+
+
     }
 }
--- CS412Final_Simeonov/Repository/OrderRepository.cs
+++ CS412Final_Simeonov/Repository/OrderRepository.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using CS412Final_Simeonov.Models;
+using CS412Final_Simeonov.Repository.Interfaces;
+using CS412Final_Simeonov.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace CS412Final_Simeonov.Repository
 {
-    public class OrderRepository
+    public class OrderRepository : IOrderRepository
     {
+        public bool addItemToOrder(Item item)
+        {
+            return OrderDAL.
+        }
+
+        public List<Order> getAllOrdersForUser(User user)
+        {
+            Or
+        }
+
+        public bool saveOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
