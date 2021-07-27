bb4861ff9e6bea1dd4cf98f048e82f72db721189
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-07-26 22:58:47 -0500

    Added User Bussiness Logic Interface

---
+++ CS412Final_Simeonov/BLL/Interfaces/IUserBLL.cs
@@ -0,0 +1,15 @@
+﻿using CS412Final_Simeonov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS412Final_Simeonov.BLL.Interfaces
+{
+    public interface IUserBLL
+    {
+        User FindUserByEmailAndPassword(string email, string password);
+        User SaveUser(User user);
+    }
+}
