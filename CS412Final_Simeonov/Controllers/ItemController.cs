207d539a22c6d58b6b4361188d7b8962a1c37ef7
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-07-27 23:06:25 -0500

    Added all the routes to the controller and created ItemRequest class

--- CS412Final_Simeonov/Controllers/ItemController.cs
+++ CS412Final_Simeonov/Controllers/ItemController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CS412Final_Simeonov.BLL;
+using CS412Final_Simeonov.BLL.Interfaces;
+using CS412Final_Simeonov.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,6 +13,55 @@ namespace CS412Final_Simeonov.Controllers
     [RoutePrefix("api/items")]
     public class ItemController : ApiController
     {
-        
+        private readonly IItemBLL itemBLL;
+
+        public ItemController() {
+            this.itemBLL = new ItemBLL();
+        }
+
+        [HttpGet]
+        public List<Item> GetAllItems()
+        {
+            return itemBLL.GetAllItems();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Item GetItemById(long id)
+        {
+            return itemBLL.FindItemById(id);
+        }
+
+        [HttpPost]
+        [Route("addItem")]
+        public HttpResponseMessage saveItem(ItemRequest itemRequest)
+        {
+            if (itemBLL.AddNewItem(itemRequest.Item)) {
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, "Could not save the item");
+        }
+
+        [HttpPut]
+        [Route("updateItem")]
+        public HttpResponseMessage updateItem(ItemRequest itemRequest)
+        {
+            if (itemBLL.UpdateItem(itemRequest.Item))
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, "Could not update the item");
+        }
+
+        [HttpDelete]
+        [Route("deleteItem")]
+        public HttpResponseMessage deleteItem(ItemRequest itemRequest)
+        {
+            if (itemBLL.DeleteItem(itemRequest.Item))
+            {
+                return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, "Could not delete the item");
+        }
     }
 }
