d6c6adb7fa588876410372e9b02870dfa7a64bc6
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-07-27 21:03:04 -0500

    Added new Item Controller and deleted the user controller

---
+++ CS412Final_Simeonov/Controllers/ItemController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CS412Final_Simeonov.Controllers
+{
+    [RoutePrefix("api/items")]
+    public class ItemController : ApiController
+    {
+        
+    }
+}
