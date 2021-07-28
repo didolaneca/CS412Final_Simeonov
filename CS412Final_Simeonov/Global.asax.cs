46b3a55125c4fd073d4daad0793372d77ddb80ae
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-07-27 19:38:42 -0500

    Added global configuration

--- CS412Final_Simeonov/Global.asax.cs
+++ CS412Final_Simeonov/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CS412Final_Simeonov.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
@@ -16,6 +18,7 @@ namespace CS412Final_Simeonov
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
