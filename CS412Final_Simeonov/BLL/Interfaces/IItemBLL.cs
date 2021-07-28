4032b4fceb40694dbe073db75600bbaeb3bd157e
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-07-27 21:09:23 -0500

    Added item bussiness logic interface

---
+++ CS412Final_Simeonov/BLL/Interfaces/IItemBLL.cs
@@ -0,0 +1,18 @@
+﻿using CS412Final_Simeonov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS412Final_Simeonov.BLL.Interfaces
+{
+    interface IItemBLL
+    {
+        List<Item> GetAllItems();
+        Item FindItemById(long id);
+        Boolean AddNewItem(Item item);
+        Boolean UpdateItem(Item item);
+        Boolean DeleteItem(Item item);
+    }
+}
